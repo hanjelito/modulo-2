367bbdc6343dd7c108bd72c87a8db682b5d37ebb
django13007 <164808086+django13007@users.noreply.github.com>
2024-04-06 17:14:43 -0500

    Modulo02

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,32 @@
+
+#include <unistd.h>
+/*#include <stdio.h>*/
+
+/*void	ft_putchar(char c)
+{
+write(1,&c,1);
+}
+*/
+void ft_putstr(char *str){
+str="MUNDOCRUEL";
+int c=0;
+while (str[c] != '\0')
+{
+
+ft_putchar(str[c]);
+c++;
+
+
+}
+
+/*
+}
+
+
+int main(){
+     char cad;
+     ft_putstr(&cad);
+    
+    return 0;
+}
+*/
