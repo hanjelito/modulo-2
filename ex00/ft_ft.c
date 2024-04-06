104f5031d325fb45372c6856a539a28c04a7bab1
django13007 <164808086+django13007@users.noreply.github.com>
2024-04-06 17:35:41 -0500

    mod02

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -1,26 +1,5 @@
 #include <unistd.h>
-<<<<<<< HEAD
-=======
-#include <stdio.h>
->>>>>>> 4722c8719ccb71f116866314c9dad3a61ff8a290
-
 void ft_ft(int *nbr)
 {
 *nbr=42;
-
 }
-
-
-<<<<<<< HEAD
-
-=======
-int main(void)
-{
-  int n=1;
-  int  *p1;
-  p1=&n;
-  ft_ft(p1);
-  printf("%d",n);
-
-}
->>>>>>> 4722c8719ccb71f116866314c9dad3a61ff8a290
